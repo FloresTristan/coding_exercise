30a98d231963f19f73d889cc6b8616a6b1f989d2
FloresTristan <147235296+FloresTristan@users.noreply.github.com>
2025-07-18 10:35:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// index.php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Applicant Name</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Applicant Name</h1>
+    <h2>Table of Contents</h2>
+    <ul>
+        <li><a href="exercise1.php">Part 1 - Pattern 1</a></li>
+        <li><a href="exercise2.php">Part 1 - Pattern 2</a></li>
+        <li><a href="exercise3.php">Part 1 - Pattern 3</a></li>
+        <li><a href="exercise4.php">Part 1 - Pattern 4</a></li>
+        <li><a href="exercise5.php">Part 2 - Random Character Table</a></li>
+        <li><a href="exercise6.php">Part 2 - Unique Number Grid</a></li>
+        <li><a href="exercise7.php">Part 2 - Stack (FILO)</a></li>
+        <li><a href="exercise8.php">Part 2 - Queue (FIFO)</a></li>
+        <li><a href="exercise9.php">Part 2 - Consonant Grid</a></li>
+        <li><a href="exercise10.php">Part 2 - Bubble Sort Fix</a></li>
+    </ul>
+</body>
+</html>
